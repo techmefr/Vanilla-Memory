c840ad0e4d4d335c85598c39b5b9b8cefd9b94c9
techmefr <compigni.gaetan@gmail.com>
2024-03-18 16:20:33 +0100

    add alert in 0

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const cards = document.querySelectorAll(".memory-card");
 let click = cards.length + 10;
 let counter = 0;
+
 function loadArrayLength() {
   document.querySelector(".score-value").innerText = click;
 }
 
 function clickEvent() {
   click -= 1;
-  if (click >= 0) {
-    const scoreElement = document.querySelector(".score-value");
-    scoreElement.textContent = click;
-  } else {
+  const scoreElement = document.querySelector(".score-value");
+  scoreElement.textContent = click;
+
+  if (click === 0) {
     alert("Fin du jeu");
-    return;
   }
 }
 
 function pairCount(elmnt) {
-  if (elmnt % 2 == 0) {
+  if (elmnt % 2 === 0) {
     clickEvent();
   }
 }
